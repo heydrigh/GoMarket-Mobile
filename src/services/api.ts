05516ecd03c5f35c27d8dc61c10ddcbf58bffe68
heydrigh <heydrigh@gmail.com>
2020-09-22 12:59:24 -0300

    adding cart functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://192.168.0.15:3333',
 });
 
 export default api;
+
+// yarn json-server --host 192.168.0.10 server.json -p 3333
